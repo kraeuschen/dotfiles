4641559af65ef3a3984c4e7411177d59fb371e45
Jan Krause <jan.krause@tvsquared.com>
2018-07-30 11:24:14 +0200

    update of dotfiles

--- zshrc.sh
+++ zshrc.sh
@@ -10,12 +10,6 @@ export LC_ALL="en_US.UTF-8"
 # User configuration
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin:/usr/local/bin:/opt/local/bin:/opt/local/sbin:$PATH
 
-# Virtual Envs
-export WORKON_HOME=$HOME/.virtualenvs
-if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
-	. /usr/local/bin/virtualenvwrapper.sh
-fi
-
 # Aliases
 if [ -f ~/.bash_aliases ]; then
 	. ~/.bash_aliases
@@ -49,3 +43,29 @@ setopt share_history # share command history data
 export EDITOR='vim'
 export VISUAL='vim'
 export PAGER='less'
+
+export TF_VAR_user='jan'
+
+# set where virutal environments will live
+export WORKON_HOME=$HOME/.virtualenvs
+# ensure all new environments are isolated from the site-packages directory
+export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
+# use the same directory for virtualenvs as virtualenvwrapper
+export PIP_VIRTUALENV_BASE=$WORKON_HOME
+# makes pip detect an active virtualenv and install to it
+export PIP_RESPECT_VIRTUALENV=true
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
+
+if [[ -r /usr/local/bin/virtualenvwrapper.sh ]]; then
+        source /usr/local/bin/virtualenvwrapper.sh
+else
+	echo "WARNING: Can't find virtualenvwrapper.sh"
+fi
+
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/jankrause/Downloads/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/jankrause/Downloads/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/jankrause/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then source '/Users/jankrause/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
